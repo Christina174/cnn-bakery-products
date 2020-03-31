84878b8926296631c29c176671937b4f1f1b512b
Christina174 <christinad@mail.ru>
2020-03-31 22:32:54 +0300

    update dataset

--- transformed/generator.sh
+++ transformed/generator.sh
@@ -1,18 +1,21 @@
 #bin
 cd train
 echo "filename,label" > ../train.csv
-find $PWD | sort | grep cruassan | awk '{print $1",cruassan"}' >> ../train.csv
-find $PWD | sort | grep ponchik | awk '{print $1",ponchik"}' >> ../train.csv
-find $PWD | sort | grep zavitok | awk '{print $1",zavitok"}' >> ../train.csv
+find $PWD | sort | grep cruassan | grep jpg |awk '{print $1",cruassan"}' >> ../train.csv
+find $PWD | sort | grep polki | grep jpg | awk '{print $1",polki"}' >> ../train.csv
+find $PWD | sort | grep ponchik | grep jpg |awk '{print $1",ponchik"}' >> ../train.csv
+find $PWD | sort | grep zavitok | grep jpg |awk '{print $1",zavitok"}' >> ../train.csv
 cd ../
 cd val 
 echo "filename,label" > ../val.csv
-find $PWD | sort | grep cruassan | awk '{print $1",cruassan"}'>> ../val.csv
-find $PWD | sort | grep ponchik | awk '{print $1",ponchik"}' >> ../val.csv
-find $PWD | sort | grep zavitok | awk '{print $1",zavitok"}' >> ../val.csv
+find $PWD | sort | grep cruassan | grep jpg |awk '{print $1",cruassan"}'>> ../val.csv
+find $PWD | sort | grep polki | grep jpg | awk '{print $1",polki"}' >> ../val.csv
+find $PWD | sort | grep ponchik | grep jpg |awk '{print $1",ponchik"}' >> ../val.csv
+find $PWD | sort | grep zavitok | grep jpg |awk '{print $1",zavitok"}' >> ../val.csv
 cd ../
 cd test 
 echo "filename,label" > ../test.csv
-find $PWD | sort | grep cruassan | awk '{print $1",cruassan"}'>> ../test.csv
-find $PWD | sort | grep ponchik | awk '{print $1",ponchik"}' >> ../test.csv
-find $PWD | sort | grep zavitok | awk '{print $1",zavitok"}' >> ../test.csv
+find $PWD | sort | grep cruassan | grep jpg |awk '{print $1",cruassan"}'>> ../test.csv
+find $PWD | sort | grep polki | grep jpg | awk '{print $1",polki"}' >> ../test.csv
+find $PWD | sort | grep ponchik | grep jpg |awk '{print $1",ponchik"}' >> ../test.csv
+find $PWD | sort | grep zavitok | grep jpg |awk '{print $1",zavitok"}' >> ../test.csv
